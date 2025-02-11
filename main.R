548f566689ee3e8140236b033530f4cfbee3ea50
Thinh Le <let9@go.stockton.edu>
2025-02-10 21:17:02 -0500

    Complete from requirement 1b to 2c

--- main.R
+++ main.R
@@ -7,13 +7,20 @@
 library(R6)      # Create classes in R
 library(readxl)  # Read excel files
 library(stringr) # Work with strings
+library(dplyr)   # Work with data
 
 
 
 # SETTINGS ---------------------------------------------------------------------
 # Note: Only general settings go here
+# File name
 INFILE <- "expt06.xlsx"
+# Should match value in cell A2
 EXPERIMENT_NAME <- "Experiment 6:  Limiting Reactants"
+# Start row index of main experiment data in the excel sheet
+MAIN_DF_START_INDEX <- 9
+# End row index of main experiment data in the excel sheet
+MAIN_DF_END_INDEX <- 32
 
 
 
@@ -70,6 +77,20 @@ get_cell_value <- function(df, cell_name) {
   return(df[row_index, column_index, drop = TRUE])
 }
 
+# Convert columns from character type to numeric type
+# column_indices is a vector of column names, e.g. c(1, 3)
+convert_columns_to_numerics <- function(df, column_indices = NULL) {
+  # If column_indices is NULL: covert all columns to numeric
+  if (is.null(column_indices)) {
+    df[] <- lapply(df, as.numeric)
+  } else {
+    # Else, only convert specified columns by indices
+    df[column_indices] <- lapply(df[column_indices], as.numeric)
+  }
+
+  return(df)
+}
+
 
 
 # CLASSES ----------------------------------------------------------------------
@@ -81,39 +102,53 @@ Base_Experiment <- R6Class(
   
   # Public properties and methods
   public = list(
-    # Store the data from file
-    df = NULL,
-    
+    # Sheet data
+    sheet_df = NULL,
+    # Main data
+    main_df = NULL,
+
     # Constructor - Read excel file as default
     initialize = function(
-      # input file path
+      # Input file path
       file_path,
       # TRUE to use the first row as column names
       col_names = FALSE
     ) {
-      # self keyword helps reference to class properties [and methods ?]
-      self$df <- read_excel(file_path, col_names=col_names)
+      # 'self' keyword helps reference to class properties [and methods ?]
+      self$sheet_df <- read_excel(file_path, col_names=col_names)
 
       # Requirement 1.a: Check if the experiment has a matching name in cell A2;
       # exit if no title is found or not matching the name
       self$check_experiment_name()
     },
-    
+
     # Check if the experiment name is provided in cell A2
     # Must always check the experiment name in all experiments
     check_experiment_name = function() {
-      cell_value <- get_cell_value(self$df, "A2")
+      cell_value <- get_cell_value(self$sheet_df, "A2")
 
-      # if cell value is NA or contain only space characters
+      # Stop if experiment name in cell A2 is NA or contain only space
+      # characters
       if (is.na(cell_value) || trimws(cell_value) == "") {
         stop("The experiment name in cell A2 is not provided.")
       }
-      
+
+      # Stop if experiment name in cell A2 is not correct
       if (cell_value != EXPERIMENT_NAME) {
         stop(paste0(
           "The experiment name in cell A2 is not correct. ",
           "The value should be '", EXPERIMENT_NAME, "'."))
       }
+    },
+
+    # Get the main df by row indices
+    get_main_df = function(start_row, end_row) {
+      # Select main data from sheet data and convert it to data frame
+      self$main_df <- as.data.frame(self$sheet_df[start_row:end_row, ])
+      # Set the first column as row names
+      rownames(self$main_df) <- self$main_df[[1]]
+      # Remove the first column from the main df
+      self$main_df <- self$main_df[, -1]
     }
   )
 )
@@ -123,13 +158,106 @@ Experiment_6 <- R6Class(
   "Experiment_6",
   # Make it inherit from Base_Experiment class using the inherit property
   inherit = Base_Experiment,
-  
+
   # Public properties and methods
   public = list(
+    # Boolean vector for checking missing values in column B
+    chkB = NULL,
+    # Boolean vector for checking chemical compound mass values in
+    # column B
+    chk_mass = NULL,
+    # Boolean vector for checking missing values in column C
+    chkC = NULL,
+    # Boolean vector for checking crucible mass values in column C
+    chk_crucible = NULL,
+    # Boolean vector for checking missing values in column D
+    chkD = NULL,
+    # Boolean vector for checking ppt (precipitate ?) values in column D
+    chk_ppt = NULL,
+    # Molar mass of KOx?
+    MM_KOx = 184.23,
+    # Molar mass of CaOx?
+    MM_CaOx = 146.11,
+
     # Constructor - Read excel file
     initialize = function(file_path) {
       # Call parent's constructor
       super$initialize(file_path)
+    },
+
+    # Override the get main df in parent
+    get_main_df = function(start_row, end_row) {
+      # Call the get_main_df() function from parent
+      super$get_main_df(start_row, end_row)
+      # Rename columns
+      names(self$main_df) <- c("B", "C", "D", "E", "F", "G")
+      # Convert string data to numeric data
+      self$main_df <- convert_columns_to_numerics(self$main_df)
+    },
+
+    # Check for missing data
+    # Consition False if data is missing and True otherwise
+    check_missing_data = function() {
+      self$chkB <- !is.na(self$main_df$B)
+      self$chkC <- !is.na(self$main_df$C)
+      self$chkD <- !is.na(self$main_df$D)
+    },
+
+    # Check chemical compound mass data
+    # False if not NA and (negative or not in range (0.080, 0.340))
+    check_chemical_compound_mass = function() {
+      self$chk_mass <- !(self$main_df$B < 0 |
+         self$main_df$B < 0.080 | self$main_df$B > 0.340) & self$chkB
+    },
+
+    # Check crucible mass data
+    # False if not NA and negative
+    check_crucible_mass = function() {
+      self$chk_crucible <- !(self$main_df$C < 0) & self$chkC
+    },
+
+    # Check ppt (precipitate ?) mass data
+    # False if not NA and (negative or mass in column D < mass in column C)
+    # column C must also pass the validation
+    check_precipitate_mass = function() {
+      self$chk_ppt <- !(self$main_df$D < 0 |
+        self$main_df$D < self$main_df$C) & self$chkD & self$chkC
+    },
+
+    # E = D - C
+    calculate_chemical_compound_created_mass = function() {
+      self$main_df$E <- ifelse(
+        # If columns C and D passed the validation
+        self$chk_crucible & self$chk_ppt,
+        # Do the substractio
+        self$main_df$D - self$main_df$C,
+        # Else, assign NA
+        NA
+      )
+    },
+
+    # F = B / MM_KOx
+    calculate_KOx_moles = function() {
+      self$main_df$F <- ifelse(
+        # If column B passed the validation
+        self$chk_mass,
+        # Do the division
+        self$main_df$B / self$MM_KOx,
+        # Else, assign NA
+        NA
+      )
+    },
+
+    # G = E / MM_CaOx
+    calculate_CaOx_moles = function() {
+      self$main_df$G <- ifelse(
+        # If column E is not NA
+        !is.na(self$main_df$E),
+        # Do the division
+        self$main_df$E / self$MM_CaOx,
+        # Else, assign NA
+        NA
+      )
     }
   )
 )
@@ -139,5 +267,22 @@ Experiment_6 <- R6Class(
 # MAIN -------------------------------------------------------------------------
 # This is where the code run
 # Create an object of the Experiment_6 class using the new() function
+# Requirement 1.a
 experiment <- Experiment_6$new(INFILE)
+# Requirement 1.b:
+experiment$get_main_df(MAIN_DF_START_INDEX, MAIN_DF_END_INDEX)
+experiment$check_missing_data()
+# Requirement 1.c:
+experiment$check_chemical_compound_mass()
+# Requirement 1.d:
+experiment$check_crucible_mass()
+# Requirement 1.e:
+experiment$check_precipitate_mass()
+# Requirement 2.a:
+experiment$calculate_chemical_compound_created_mass()
+# Requirement 2.b:
+experiment$calculate_KOx_moles()
+# Requirement 2.c:
+experiment$calculate_CaOx_moles()
 
+# print(experiment$main_df)
